bdff7c9154961b77f3d747192d6c1723dfcfc525
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-13 21:36:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params[:username] == "" || params[:username] == ""
+    if params[:username] == "" || params[:password] == ""
       redirect to '/failure'
     end
     erb :login
